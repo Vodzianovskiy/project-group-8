379ec999faf9ea018df3f0b768d393ffbb9a4e59
Vodzianovskiy <vvodzanovskij@gmail.com>
2025-11-08 23:14:48 +0000

    Deploying to gh-pages from @ Vodzianovskiy/project-group-8@d4c663eeeafec0b587e776d9a16f485d70f30d39 🚀

---
+++ assets/experience-anim-Cpi76jn2.js
@@ -0,0 +1,2 @@
+document.addEventListener("DOMContentLoaded",()=>{if(window.matchMedia("(min-width: 768px)").matches){const e=document.querySelector(".experience");e&&new IntersectionObserver(s=>{s.forEach(t=>{t.isIntersecting?e.classList.add("show"):e.classList.remove("show")})},{threshold:.23}).observe(e)}});
+//# sourceMappingURL=experience-anim-Cpi76jn2.js.map
