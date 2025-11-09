4e7513156088a7152c0c90906b8e6c722416752d
Ivan <vvodzanovskij@gmail.com>
2025-11-09 19:41:48 +0100

    last update

--- src/main.js
+++ src/main.js
@@ -1,36 +1,31 @@
 document.addEventListener('DOMContentLoaded', async () => {
-  const mobileMenuCache = import('/js/mobile-menu.js');
-  const experienceAnimCache = import('/js/experience-anim.js');
-  const modalCache = import('/js/modal.js');
+  const [mobileMenuModule, experienceAnimModule, modalModule] =
+    await Promise.all([
+      import('/js/mobile-menu.js'),
+      import('/js/experience-anim.js'),
+      import('/js/modal.js'),
+    ]);
+
+  const { initMobileMenu } = mobileMenuModule;
+  const { default: initExperienceAnim } = experienceAnimModule;
+  const { openSubscriptionModal } = modalModule;
 
   const openMenuBtn = document.querySelector('.burger-buttom');
   if (openMenuBtn) {
-    openMenuBtn.addEventListener(
-      'click',
-      async () => {
-        const { initMobileMenu } = await mobileMenuCache;
-        initMobileMenu();
-      },
-      { once: true }
-    );
+    openMenuBtn.addEventListener('click', () => initMobileMenu(), {
+      once: true,
+    });
   }
 
   if (window.matchMedia('(min-width: 768px)').matches) {
-    const experienceAnimCache = import('/js/experience-anim.js');
-    const initExperienceAnim = (await experienceAnimCache).default;
     initExperienceAnim();
   }
 
   const subscribeBtn = document.querySelector('.input_button[type="submit"]');
   if (subscribeBtn) {
-    subscribeBtn.addEventListener(
-      'click',
-      async e => {
-        e.preventDefault();
-        const { openSubscriptionModal } = await modalCache;
-        openSubscriptionModal();
-      },
-      { once: false }
-    );
+    subscribeBtn.addEventListener('click', e => {
+      e.preventDefault();
+      openSubscriptionModal();
+    });
   }
 });
